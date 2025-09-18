cc2ed4d26ca4036c972a4c89831ab1d5cf60900d
omadokar <146622041+omadokar@users.noreply.github.com>
2025-09-18 11:53:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -260,6 +260,5 @@ app.post('/api/admin/notify', async (req, res) => {
 });
 
 // ---------------- START SERVER ----------------
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}/`));
+module.exports = app;
 
